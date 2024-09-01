a9a2acbb57ac8ba138abc86fb0e90e468e8235f6
Tuwar Gaurav <tuwargaurav8@gmail.com>
2024-09-01 20:37:35 +0530

    Add files via upload
    Check the number is between 10 and 20 using and operator

---
+++ programme 8.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int num;
+cout<<"Enter a number:";
+cin>>num;
+
+if(num>=10 && num<=20){
+
+cout<<"The number"<<num<<"is between 10 and 20.";
+
+}else{
+
+cout<<"The number"<<num<<"is not between 10 and 20.";
+
+}
+
+return 0;
+
+}
