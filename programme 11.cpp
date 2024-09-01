f29b0a0c98006ba7369da7fbe37c6266718b3c4b
Tuwar Gaurav <tuwargaurav8@gmail.com>
2024-09-01 21:04:56 +0530

    Add files via upload
    Basic arithmetic operations using binary operator

---
+++ programme 11.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int a=6, b=5;
+
+cout<<"a+b= "<<(a+b)<<endl;
+
+cout<<"a-b= "<<(a-b)<<endl;
+
+cout<<"a*b= "<<(a*b)<<endl;
+
+cout<<"a/b= "<<(a/b)<<endl;
+
+cout<<"a%b= "<<(a%b)<<endl;
+
+return 0;
+
+}
