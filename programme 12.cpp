9d30d54f35000dc38267dacd645c1ed0a953d9fe
Tuwar Gaurav <tuwargaurav8@gmail.com>
2024-09-01 21:06:03 +0530

    Add files via upload
    CPP programme to demonstrate the bitwise operator

---
+++ programme 12.cpp
@@ -0,0 +1,21 @@
+//CPP Program to demostrate the Bitwise operator
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{ int a=2, b=4;
+
+//Binary AND operator cout<<"a&b is "<<(a&b)<<endl;
+
+//Binary OR operator
+
+cout<<"a|b is "<<(a|b)<<endl;
+
+//One's Complement operator cout<<"-(a) is "<<~(a)<<endl;
+
+return 0;
+
+}
