7b887193a609a9706eb743575d7887a411ffdec0
Tuwar Gaurav <tuwargaurav8@gmail.com>
2024-09-01 20:39:03 +0530

    Add files via upload
    Programme to compare two integer using relational Operator

---
+++ programme 9.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int a=6,b=4;
+
+cout<<"a > b is "<<(a > b)<<endl;
+
+//Greater than operator
+
+cout<<"a > b is "<<(a > b)<<endl;
+
+//Greater than or Equal to operator cout<<"a >= b is "<<(a > b)<<endl;
+
+//Lesser than operator
+
+cout<<"a < b is "<<(a<=b)<<endl;
+
+//Lesser than or Equal to operator
+
+cout<<"a <= b is "<<(a <= b)<<endl;
+
+cout<<"a 1= b is "<<(a = b)<<endl;
+
+return 0;
+
+}
